e8035544199989844ce4bc6d026d9bac187bc3b2
Vasilev-Stefan <sbasilev@gmail.com>
2025-10-02 19:03:49 +0300

    Added static router

--- src/index.js
+++ src/index.js
@@ -10,3 +10,30 @@ app.engine('hbs', handlebars.engine())
 
 //Setting the hbs to be the default view engine
 app.set('view engine', 'hbs')
+
+
+//Creating routes
+app.get('/', (req, res) => {
+    res.send('Home page')
+    //TODO - display the home.html
+})
+
+app.get('/about', (req, res) => {
+    res.send('About page')
+    //TODO - render the about page.
+})
+
+app.get('/create', (req, res) => {
+    res.send('Create page')
+    //TODO - render the create page.
+})
+
+app.get('/details/:id', (req, res) => {
+    res.send(`Details page for ID -> ${req.params.id}`)
+    //TODO - render the details page with conditional data
+})
+
+app.all('/*splat', (req, res) => {
+    res.send('Page not found')
+    //TODO - render the 404 page
+})
