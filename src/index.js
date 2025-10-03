5078561d6b68fd31a78fc6fb94fed832af7e0e88
Vasilev-Stefan <sbasilev@gmail.com>
2025-10-03 16:18:09 +0300

    Fixed the css path
    Added home.hbs file for displaying the home page

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 //Creating the server with express
 const app = express();
 app.listen(5000, console.log('The server is listening on http://localhost:5000...'))
 
 //Added view engine to the server
-app.engine('hbs', handlebars.engine())
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+    defaultLayout: 'main'
+}))
 
 //Setting the hbs to be the default view engine
 app.set('view engine', 'hbs')
 
+//Default css and image to be displayed
+app.use(express.static(path.join(__dirname, 'static')))
+
 
 //Creating routes
 app.get('/', (req, res) => {
-    res.send('Home page')
+    res.render('home')
     //TODO - display the home.html
 })
 
