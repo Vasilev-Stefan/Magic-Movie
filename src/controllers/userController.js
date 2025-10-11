b490a03d276d68ef621ad0acb7a88e60c3e2ba87
Vasilev-Stefan <sbasilev@gmail.com>
2025-10-11 18:06:46 +0300

    Add route to register page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,4 +6,8 @@ userController.get('/login', (req, res) => {
     res.render('login', {pageTitle: 'Login'})
 })
 
+userController.get('/register', (req, res) => {
+    res.render('register', {pageTitle: 'Register'})
+})
+
 export default userController;
