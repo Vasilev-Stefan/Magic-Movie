9c6e6f2dabe154976bd2dfd17b4ff0293362e7f0
Vasilev-Stefan <sbasilev@gmail.com>
2025-10-20 23:37:20 +0300

    Add validators to the Cast model

--- config/models/Cast.js
+++ config/models/Cast.js
@@ -1,10 +1,27 @@
 import mongoose from 'mongoose'
 
 const castSchema = mongoose.Schema({
-    name: {type: String, required: true},
-    age: {type: Number, min: [1, "The minimum age must be 1"], max: [99, "The age can not be higher than 99"]},
-    born: {type: String, required: true},
-    nameInMovie: {type: String, required: true},
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        match: [/[a-zA-Z0-9\ ]+/, 'Invalid charachers in the name']
+    },
+    age: {
+        type: Number,
+        required: [true, 'Age is required'],
+        min: [1, "The minimum age is 1"],
+        max: [120, "The age can not be higher than 120"]},
+    born: {
+        type: String,
+        required: [true, 'Born location is required'],
+        minLength: [10, 'The born location must be atleast 10 charachers long'],
+        match: [/[a-zA-Z0-9\ ]+/, 'Invalid charachers in the location']
+    },
+    nameInMovie: {
+        type: String,
+        required: [true, 'Name is movie is required'],
+        minLength: [5, "The character's name must be atleast 5 characters long"]
+    },
     castImage: {type: String, required: true, validate: {validator: function (value) {return /^(https?)/.test(value)}, message: props => `${props.value} is not a valid URL. It must start with "http://" or "https://".`}},
     movie: {type: mongoose.Types.ObjectId, ref: 'Movie'}
 })
