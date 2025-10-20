db78c1c217a1355798f42748416f5638aa33e4d1
Vasilev-Stefan <sbasilev@gmail.com>
2025-10-20 23:30:41 +0300

    Added validators to the Movie model

--- config/models/Movie.js
+++ config/models/Movie.js
@@ -1,13 +1,38 @@
 import mongoose from 'mongoose'
 
 const movieSchema = new mongoose.Schema({
-    title: {type: String, required: true},
+    title: {
+        type: String,
+        required: [true, 'Title is required'],
+        minLength: [5, 'The title must be more than 4 charachers long'],
+        match: [/[a-zA-Z0-9\ ]+/, 'Invalid charachers in the title of the movie']
+    },
     category: {type: String, required: true},
-    genre: {type: String, required: true},
-    director: {type: String, required: true},
-    year: {type: Number, required: true, max: [2025, "The year can't be more than 2025"], min: [1888, "The first movie was created in the 1888"]},
-    rating: {type: Number, required: true, max: [10, "The rating can't be higher than 10"], min: [1, "The rating can't be lower than 1"]},
-    description: {type: String, required: true, maxLength: 200},
+    genre: {
+        type: String,
+        required: [true, 'Genre is required'],
+        minLength: [5, 'The genre must be more than 4 charachers long'],
+        match: [/[a-zA-Z0-9\ ]+/, 'Invalid charachers in the genre of the movie']
+    },
+    director: {
+        type: String,
+        required: [true, 'Director is required'],
+        minLength: [5, "Director's name must be more than 4 charachers long"],
+        match: [/[a-zA-Z0-9\ ]+/, "Invalid charachers in the director's name"]
+    },
+    year: {
+        type: Number,
+        required: [true, 'Year is required'],
+        max: [2025, "The year can't be more than 2025"],
+        min: [1900, "The year can't be lower than 1900"]
+    },
+    rating: {type: Number, required: [true, 'Rating is required'], max: [5, "The rating can't be higher than 5"], min: [1, "The rating can't be lower than 1"]},
+    description: {
+        type: String,
+        required: [true, 'Description is required'],
+        minLength: [20, 'The description must be atleast 20 charachers'],
+        match: [/[a-zA-Z0-9\ ]+/, "Invalid charachers in the description"]
+    },
     imageURL: {type: String, required: true, validate: {validator: function (value) {return /^(https?)/.test(value)}, message: props => `${props.value} is not a valid URL. It must start with "http://" or "https://".`}},
     cast: [{type: mongoose.Types.ObjectId, ref: 'Cast'}],
     creatorId: {type: mongoose.Types.ObjectId, ref: 'User'}
