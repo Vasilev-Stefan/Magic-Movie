dddc03e46b0d1661535fe27e93ab809f3d640145
Vasilev-Stefan <sbasilev@gmail.com>
2025-10-20 21:43:28 +0300

    Add validations to user model

--- config/models/User.js
+++ config/models/User.js
@@ -2,8 +2,17 @@ import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
 
 const userSchema = new mongoose.Schema({
-    email: {type: String},
-    password: {type: String}
+    email: {
+        type: String,
+        unique: true,
+        match: [/@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/, 'This email is invalid'],
+        minLength: [10, 'Email is too short'] 
+    },
+    password: {
+        type: String,
+        match: [/[a-zA-Z0-9]+/, 'Invalid characters in password'],
+        minLength: [6, 'Password should be atleast 6 charachers long']
+    }
 })
 
 userSchema.pre('save', async function () {
